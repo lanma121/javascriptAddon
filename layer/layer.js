d0d164d8cd5aa61d060b99978b70bad6f0f8e9b1
lanma121 <786716597@qq.com>
2015-06-11 19:25:45 +0800

    完善弹框

--- layer/layer.js
+++ layer/layer.js
@@ -3,24 +3,48 @@
  *
  * 使用时必须先引用jQuery
  * 信息弹框
- * title: 信息弹框标题
- * content: 信息内容
- * callback : 回调函数，弹框加载完成后的函数
  *注意：不同的项目要改变layer.htm 和 关闭图片 的路径
+ *
+ * $.layer({
+            title:"gerg",//设置弹框标题
+            content:div, //设置弹框内容
+            confirms:[ //设置确认取消按钮
+                {"bnt":"确认","fun":function(){//显示第一个按钮，bnt为按钮的名称，fun为点击按钮执行的函数
+                        alert("确认");
+                }},
+                {"bnt":"取消","fun":function(){
+                    alert("取消");
+                }}],
+            callback:function (obj) {//弹框成功的回调函数
+                //alert("弹出成功！！！" +obj);
+            }
+        });
+ *
  */
 (function($){
+
     $.extend({
-        layer: function( title,content,callback ) {
+        layer: function(obj) {
             $.get('./layer.htm', function(layerDiv) {
-                console.log(content);
+                console.log(obj.content);
+                $("#layerTmp").remove();
                 $("body").append(layerDiv);
-                console.log(layerDiv);
-                $(".layui-layer-title").text(title);
-                $("#layui-layer-content").append(content);
+                if(obj.title)$(".layui-layer-title").text(obj.title);
+                if(obj.content)$("#layui-layer-content").append(obj.content);
+                if(obj.confirms){
+                    $.each(obj.confirms,function(i,v){
+                        var css = i%2==0 ? "bnt1":"bnt2"
+                        $(".layui-layer-btn").append("<a class="+css+">"+ v.bnt+"</a>");
+                        $(".layui-layer-btn a:eq("+i+")").on("click",function(){
+                            v.fun();
+                        });
+                    });
+                }
+                if(obj.callback){obj.callback($("#layui-layer").attr("id"))};
                 $("#closeLayer").on("click",function(){
                     $("#layerTmp").remove();
                 });
-                if(callback){callback()};
+
             });
         }
     });
