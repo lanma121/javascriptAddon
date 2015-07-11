2f24eecd7b7d01b508780764f4c25231f6bbf29c
lanma121 <786716597@qq.com>
2015-07-11 17:41:46 +0800

    修改 dragEvent
    修改 dragEvent

--- event/dragEvent/drag2.js
+++ event/dragEvent/drag2.js
@@ -1,4 +1,7 @@
 /**
+ * Created by Administrator on 2015/7/9 0009.
+ */
+/**
  * Created by Administrator on 2015/6/30 0030.
  */
 
@@ -30,20 +33,27 @@
 
         var opts = $.extend({},defaults,options);
         return this.each(function(){
-            var mousedownLocation = {};
+            var mouseLocation = {};
             var moveEle = $(this);//移动的元素
+            var replaceEle = null;//替换移动的元素
             var focuEle = opts.focuEle ? $(opts.focuEle,moveEle) : moveEle ;//点击哪个元素，以触发移动。该元素需要是被移动元素的子元素（比如标题等）
             if(!focuEle || focuEle.length<=0){
                 alert('focuEle is not found! the element must be a child of '+this.id);
                 return false;
             }
             focuEle.css('cursor','move'); //改变鼠标形状
+
+            function location(obj){
+                //移动方向：可以是不限定、垂直、水平。
+                opts.dragDirection=='all' ? obj.css({'left':mouseLocation.moveX,'top':mouseLocation.moveY}) : opts.dragDirection=='vertical' ?
+                    obj.css('top',mouseLocation.moveY) : opts.dragDirection=='horizontal' ? obj.css('left',mouseLocation.moveX) : false;
+            }
             function mousemoveEve(e){
                 e.preventDefault();
                 //被移动元素的新位置，实际上鼠标当前位置与原位置之差
                 //实际上，被移动元素的新位置，也可以直接是鼠标位置，这也能体现拖拽，但是元素的位置就不会精确。
-                var moveX = e.pageX - mousedownLocation.X;
-                var moveY = e.pageY - mousedownLocation.Y;
+                var moveX = e.pageX - mouseLocation.moveEleX;
+                var moveY = e.pageY - mouseLocation.moveEleY;
                 //是否限定在某个区域中移动.
                 //fixarea格式: [x轴最小值,x轴最大值,y轴最小值,y轴最大值]
                 if(opts.fixarea){
@@ -60,22 +70,39 @@
                         moveY=opts.fixarea.bottom;
                     }
                 }
-                //移动方向：可以是不限定、垂直、水平。
-                opts.dragDirection=='all' ? moveEle.css({'left':moveX,'top':moveY}) : opts.dragDirection=='vertical' ?
-                    moveEle.css('top',moveY) : opts.dragDirection=='horizontal' ? moveEle.css('left',moveX) : false;
+                mouseLocation.moveX = moveX;
+                mouseLocation.moveY = moveY;
+                location(replaceEle);
                 if(opts.callback){//如果有回调
                     opts.callback.call(opts.callback,moveX,moveY);//将dragParams作为参数传递
                 }
             }
 
             function mouseupEve(e){
-                $(document).off({"mousemove":mousemoveEve,'mouseup':mouseupEve});
+                try {
+                    $(document).off({"mousemove":mousemoveEve,'mouseup':mouseupEve});
+                    replaceEle.remove();
+                    location(moveEle);
+                } catch(a) {
+                    $(document).off({"mousemove":mousemoveEve,'mouseup':mouseupEve});
+                }
+
+
             }
             function mousedownEve(e){
                 e.preventDefault();
+                var
+                    left =  moveEle.position().left ,
+                    top = moveEle.position().top ,
+                    width = moveEle.outerWidth() - 6,
+                    height = moveEle.outerHeight() - 6;
+                mouseLocation.moveEleX = e.pageX - left;  //鼠标位置相对于拖拽对象左上角的位置filter:alpha(opacity=50);opacity:0.5;
+                mouseLocation.moveEleY = e.pageY - top;   //鼠标位置相对于拖拽对象左上角的位置
+                var styleStr="style='position:absolute;top:"+top+"px;left:"+left+"px;width:"+width+"px;height:"+height+"px;" +
+                    "cursor:move;z-index:19891016;background: rgba(255, 255, 255,.3);border:3px solid rgba(0, 0, 0,.5);filter:alpha(opacity=50);'";
+                replaceEle = $("<div id='mastLayer' "+styleStr+"></div>");
+                moveEle.offsetParent().append(replaceEle);
                 $(document).on({"mousemove":mousemoveEve,'mouseup':mouseupEve});
-                mousedownLocation.X = e.pageX - moveEle.position().left;
-                mousedownLocation.Y = e.pageY - moveEle.position().top;
             }
             focuEle.on('mousedown',mousedownEve);
         });
