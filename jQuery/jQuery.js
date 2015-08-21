d6e4a7b8da52c47f6fe869be4c32a0c994beb897
lanma121 <786716597@qq.com>
2015-08-21 17:30:50 +0800

    jquery
    jquery

---
+++ jQuery/jQuery.js
@@ -0,0 +1,123 @@
+/**
+ * Created by Administrator on 2015/7/1 0001.
+ */
+
+(function( global, factory ) {
+
+    if ( typeof module === "object" && typeof module.exports === "object" ) {
+        // For CommonJS and CommonJS-like environments where a proper window is present,
+        // execute the factory and get jQuery
+        // For environments that do not inherently posses a window with a document
+        // (such as Node.js), expose a jQuery-making factory as module.exports
+        // This accentuates the need for the creation of a real window
+        // e.g. var jQuery = require("jquery")(window);
+        // See ticket #14549 for more info
+        module.exports = global.document ?
+            factory( global, true ) :
+            function( w ) {
+                if ( !w.document ) {
+                    throw new Error( "jQuery requires a window with a document" );
+                }
+                return factory( w );
+            };
+    } else {
+        factory( global );
+    }
+
+// Pass this if window is not defined yet
+}(typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
+    var
+        version = "1.11.3",
+        // Define a local copy of jQuery
+        jQuery = function( selector, context ) {
+            // The jQuery object is actually just the init constructor 'enhanced'
+            // Need init if jQuery is called (just allow error to be thrown if not included)
+            return new jQuery.fn.init( selector, context );
+        },
+        jQuery.fn = jQuery.prototype = {
+        // The current version of jQuery being used
+        jquery: version,
+
+        constructor: jQuery,
+
+        // Start with an empty selector
+        selector: "",
+
+        // The default length of a jQuery object is 0
+        length: 0,
+
+        toArray: function() {
+            return slice.call( this );
+        },
+
+        // Get the Nth element in the matched element set OR
+        // Get the whole matched element set as a clean array
+        get: function( num ) {
+            return num != null ?
+
+                // Return just the one element from the set
+                ( num < 0 ? this[ num + this.length ] : this[ num ] ) :
+
+                // Return all the elements in a clean array
+                slice.call( this );
+        },
+
+        // Take an array of elements and push it onto the stack
+        // (returning the new matched element set)
+        pushStack: function( elems ) {
+
+            // Build a new jQuery matched element set
+            var ret = jQuery.merge( this.constructor(), elems );
+
+            // Add the old object onto the stack (as a reference)
+            ret.prevObject = this;
+            ret.context = this.context;
+
+            // Return the newly-formed element set
+            return ret;
+        },
+
+        // Execute a callback for every element in the matched set.
+        // (You can seed the arguments with an array of args, but this is
+        // only used internally.)
+        each: function( callback, args ) {
+            return jQuery.each( this, callback, args );
+        },
+
+        map: function( callback ) {
+            return this.pushStack( jQuery.map(this, function( elem, i ) {
+                return callback.call( elem, i, elem );
+            }));
+        },
+
+        slice: function() {
+            return this.pushStack( slice.apply( this, arguments ) );
+        },
+
+        first: function() {
+            return this.eq( 0 );
+        },
+
+        last: function() {
+            return this.eq( -1 );
+        },
+
+        eq: function( i ) {
+            var len = this.length,
+                j = +i + ( i < 0 ? len : 0 );
+            return this.pushStack( j >= 0 && j < len ? [ this[j] ] : [] );
+        },
+
+        end: function() {
+            return this.prevObject || this.constructor(null);
+        },
+
+        // For internal use only.
+        // Behaves like an Array's method, not like a jQuery method.
+        push: push,
+        sort: deletedIds.sort,
+        splice: deletedIds.splice
+    };
+
+})
+
